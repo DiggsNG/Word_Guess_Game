d1ec5119bd0bd7f0f9b03ddd24495efd19a13f95
DiggsNG <42653979+DiggsNG@users.noreply.github.com>
2018-12-22 17:53:49 -0800

    Updated Index.js with inquirer logic
    I have added the index inquirer logic

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//require("dotenv").config();
 var Word = require ("./Word.js");
 var continents = ["Africa","Antarctia","Asia","Australia","Ocenia","Europe","North America","South America"];
 
@@ -20,6 +19,27 @@ function GameOn (location) {
 };
 
 GameOn.prototype.assessLetter = function(guessedItLetter) {
+  var answers = function() {
+    // if statement to ensure that our questions are only asked five times
+    if (count < 10) {
+      // runs inquirer and asks the user a series of questions whose replies are
+      // stored within the variable answers inside of the .then statement
+      inquirer.prompt([
+        {
+          guesit: "character",
+          message: "Please chose a character"
+        } 
+      ])
+      .then(function(answers) {
+        // initializes the variable newProgrammer to be a programmer object which will take
+        // in all of the user's answers to the questions above
+        var newProgrammer = new Programmer(
+          answers.guestIt),
+        })  
+        answers.guestIt = guessedItLetter;
+    } else {
+      answers.guestIt = guessedItLetter;
+          }}
     //Check Key pressed.
     var characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
@@ -65,4 +85,5 @@ GameOn.prototype.assessLetter = function(guessedItLetter) {
   // module.exports = GameOn;
   var game = new GameOn(continents);
   // console.log(game);
-  game.assessLetter();
+  game.assessLetter()};
+
