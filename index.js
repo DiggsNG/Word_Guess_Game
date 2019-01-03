ce5ead0c54a836798099ca870ca32940d347281b
DiggsNG <42653979+DiggsNG@users.noreply.github.com>
2019-01-03 00:35:15 -0800

    Updated index
    This is an updated index file for the work guess application

--- index.js
+++ index.js
@@ -1,89 +1,84 @@
-var Word = require ("./Word.js");
-var continents = ["Africa","Antarctia","Asia","Australia","Ocenia","Europe","North America","South America"];
-
-function GameOn (location) {
-    //Selection  of a random word from location array.
-    this.catchWord = location[Math.floor(Math.random() * location.length)];
-
-    //Display blanks based on word selection
-    this.word = new Word(this.catchWord);
-
-    //Booleans, counters, and arrays supporting this word game.
-    this.guesses = [];
-    this.badGuesses = [];
-    this.displayBadGuesses = "";
-    this.attempsRemaining = 20;
-    this.gameisDone = false;
+var wordclass = require("./word.js");
+var inquirer = require("inquirer");
+var colors = require("colors");
+var guesses_left = 10;
+var game_over = false;
+var letters_guessed = new Set();
 
 
-};
+var word = new wordclass();
 
-GameOn.prototype.assessLetter = function(guessedItLetter) {
-  var answers = function() {
-    // if statement to ensure that our questions are only asked five times
-    if (count < 10) {
-      // runs inquirer and asks the user a series of questions whose replies are
-      // stored within the variable answers inside of the .then statement
-      inquirer.prompt([
+var letters_left = word.letters.length;
+
+colors.setTheme ({
+    silly: "rainbow",
+    input: "grey",
+    verbose: "grey",
+    prompt: "grey",
+    help: "cyan",
+    warn: "yellow",
+    debug: "blue",
+    error: "red"
+
+});
+
+
+
+function startgame(){
+    word.showLetters();
+    inquirer.prompt([
         {
-          guesit: "character",
-          message: "Please chose a character"
-        } 
-      ])
-      .then(function(answers) {
-        // initializes the variable newProgrammer to be a programmer object which will take
-        // in all of the user's answers to the questions above
-        var newProgrammer = new Programmer(
-          answers.guestIt),
-        })  
-        guessedItLetter = answers.guestIt;
-    } else {
-      guessedItLetter = answers.guestIt;
-          }}
-    //Check Key pressed.
-    var characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-    //If the guess is not a character
-    if(characters.indexOf(guessedItLetter) === -1){
-    console.log("Please enter a letter.".warn);
-      }else{
-        //Check if letter has already been guessed
-          if (this.guesses.indexOf(guessedItLetter) > -1){
-          console.log("You've already guessed that letter.".warn);
-          }
-        //Check that the letter is incorrect
-          else if(this.catchWord.toUpperCase().indexOf(guessedItLetter) === -1) {
-            this.badGuesses.push(guessedLetter.red);
-            this.displayBadGuesses = this.badGuesses.join("");
-            this.attempsRemaining --;
-            console.log("Your guess was incorrect.".red);
-          }
-        //Letter is correct
-            else {
-              this.word.checkIfWordContains(guessedItLetter);
-              this.displayWord = this.displayWord();
-              console.log ("Your guess was correct.".green);
-            };
-            //record all valid guest
-            this.guesses.push(guessedItLetter);
-          };  
-
-
-    };
-  GameOn.prototype.evaluateGameState = function() {
-      if(this.displayWord.indexOf("_") === -1) {
-          //Letters are guessed, win
-          console.log("You win".green.bold);
-          this.gameisDone = true;
+            type: "input",
+            name: "letter",
+            message: "Guess a letter".input,
         }
-         else if (this.attempsRemaining <1) {
-          //Attemps Remaining...lose
-          console.log(" You have lost.".red.bold);
-          this.gameisDone = true;
-      };
-  };
-  // module.exports = GameOn;
-  var game = new GameOn(continents);
-  // console.log(game);
-  game.assessLetter()};
+    ]).then( function(response){
+        guess(response.letter);
+        if(!game_over){
+            startgame();
+        }
+    })
+}
+console.log("Welcome to the Word Guess Game!!".silly.bold);
+startgame();
 
+//Logic for responding to guesses
+function guess(letter){
+    if(letter.length === 1){
+        if(!letters_guessed.has(letter)){
+            letters_guessed.add(letter);
+        }
+        else{
+            console.log("Letter has already been guessed!\n".warn);
+            return;
+        }
+        var num_correct = word.checkGuess(letter);
+        console.log(num_correct);
+        if( num_correct > 0){
+            console.log("Correct!\n");
+            letters_left -= num_correct;
+        }
+        else{
+            guesses_left--;
+            console.log("Incorrect!  " + guesses_left + " guesses remaining\n");
+        }
+    
+        if(letters_left === 0){
+            console.log("You win!".silly);
+            word.showLetters();
+            game_over = true;
+        }
+    
+        if(guesses_left === 0){
+            console.log("You lose!".help);
+            console.log("Word was " + word.word)
+            game_over = true;
+        }
+
+    }
+    else{
+        console.log("You can only guess one letter at a time!\n".help);
+    }
+    
+    
+}
